8e3a0a970d4c62aa7a759ce4dde4c575ac2c3845
akmalovkuhinur05-del <akmalovkuhinur05@gmail.com>
2026-08-29 22:33:30 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,22 +1,19 @@
- 
 import asyncio
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import Command
 
-# Bot tokeningizni shu yerga qo'ying
-BOT_TOKEN = "8996873868:AAHtwvuXhsAkmW0n-B376wH25pgfQeCurp0"⁠
+API_TOKEN = 8996873868:AAHtwvuXhsAkmW0n-B376wH25pgfQeCurp0
 
-bot = Bot(token=BOT_TOKEN)
+bot = Bot(token=API_TOKEN)
 dp = Dispatcher()
 
-# /start buyrug'i uchun javob
 @dp.message(Command("start"))
 async def start_cmd(message: types.Message):
     await message.answer("Salom! Open Budget botiga xush kelibsiz!")
 
 async def main():
-    print("Bot ishga tushdi...")
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
     asyncio.run(main())
+
