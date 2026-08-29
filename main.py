087c35af4059a5903e5541c466e829682a693091
akmalovkuhinur05-del <akmalovkuhinur05@gmail.com>
2026-08-29 22:44:37 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import asyncio
+import os
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import Command
+from aiohttp import web
 
 API_TOKEN = "8996873868:AAHtwvuXhsAkmW0n-B376wH25pgfQeCurp0"
-
 
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher()
@@ -12,9 +13,20 @@ dp = Dispatcher()
 async def start_cmd(message: types.Message):
     await message.answer("Salom! Open Budget botiga xush kelibsiz!")
 
+# Render port talab qilgani uchun soxta veb-server
+async def handle(request):
+    return web.Response(text="Bot ishlayapti!")
+
 async def main():
+    app = web.Application()
+    app.router.add_get('/', handle)
+    runner = web.AppRunner(app)
+    await runner.setup()
+    port = int(os.environ.get("PORT", 8080))
+    site = web.TCPSite(runner, '0.0.0.0', port)
+    await site.start()
+    
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
     asyncio.run(main())
-
